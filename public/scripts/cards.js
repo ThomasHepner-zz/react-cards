c21c476351d7297960468c7c39be54a73d59e615
Thomas Hepner <thepner32@gmail.com>
2015-11-16 12:12:36 -0300

    Flipping cards

--- public/scripts/cards.js
+++ public/scripts/cards.js
@@ -1,19 +1,22 @@
+var data = [
+  {id: 1, question: "What's your name?", answer: "Thomas"},
+  {id: 2, question: "What's your last name?", answer: "Hepner"}
+];
+
 var Card = React.createClass({
-    // TODO: check this method and Jed Watson's classnames to make it work.
-    flipCard: function(btn) {
-        var $card = $(btn).closest('.card-container');
-        console.log($card);
-        if ($card.hasClass('hover')) {
-            $card.removeClass('hover');
-        } else {
-            $card.addClass('hover');
-        }
+    getInitialState: function() {
+      return ({flipped: false});
+    },
+    flipCard: function(e) {
+      var nextState = !this.state.flipped
+      this.setState({flipped: nextState});
     },
     render: function() {
+      var cardClasses = classNames({'manual-flip': true, 'card-container': true, 'hover': this.state.flipped})
         return (
             <div className="card">
-                <div className="col-sm-4">
-                  <div className="card-container manual-flip">
+                <div className="col-sm-6">
+                  <div className={cardClasses}>
                     <div className="card">
                       <div className="front">
                         <div className="cover">
@@ -23,7 +26,7 @@ var Card = React.createClass({
                           <div className="main">
                             <h3 className="name">Question #1</h3>
                             <p className="profession">History</p>
-                            <h3 className="text-center question">When was George Washington born?</h3>
+                            <h3 className="text-center question">{this.props.question}</h3>
                           </div>
                           <div className="footer">
                             <button className="btn btn-simple" onClick={this.flipCard}>
@@ -39,11 +42,11 @@ var Card = React.createClass({
                           </div>
                           <div className="main text-center">
                             <h3>Answer</h3>
-                            <p className="answer">George Washington was born in Boston, Massachussets, on 1848.</p>
+                            <p className="answer">{this.props.answer}</p>
                           </div>
                         </div>
                         <div className="footer">
-                          <button className="btn btn-simple" rel="tooltip" title="Flip Card" onclick="rotateCard(this)">
+                          <button className="btn btn-simple" rel="tooltip" title="Flip Card" onClick={this.flipCard}>
                             <i className="fa fa-reply"></i> Back
                           </button>
                         </div>
@@ -57,7 +60,48 @@ var Card = React.createClass({
     }
 });
 
+var Deck = React.createClass({
+  render: function() {
+    return (
+      <div className="deck">
+        <li className="list-group-item">
+          <span className="badge">14</span>
+          {this.props.name}
+        </li>
+      </div>
+    );
+  }
+});
+
+var DeckList = React.createClass({
+  render: function() {
+    return (
+      <div className="deckList">
+        <ul className="list-group">
+          <Deck name="Cards" />
+          <Deck name="History" />
+        </ul>
+      </div>
+    );
+  }
+});
+
+var DeckBox = React.createClass({
+  render: function() {
+    return (
+      <div className="deckBox">
+        <h1 className="text-center">Decks</h1>
+        <DeckList />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-    <Card />,
-    document.getElementById('content')
+    <Card question="What day is it?" answer="Today" />,
+    document.getElementById('cards-content')
+);
+ReactDOM.render(
+    <DeckBox />,
+    document.getElementById('decks-content')
 );
